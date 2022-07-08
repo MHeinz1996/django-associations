7fb1065191afc81c22052a950f9f256d81ad41e4
MHeinz1996 <michael.heinzinger@gmail.com>
2022-07-08 07:23:00 -0500

    Work in progress

--- imdb/moviedb/models.py
+++ imdb/moviedb/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+class Actor(models.Model):
+    first_name = models.CharField(max_length=255)
+    last_name = models.CharField(max_length=255)
+
+class Movie(models.Model):
+    title = models.CharField(max_length=255)
+    actors = models.ManyToManyField(Actor, through="Role")
+
+class Role(models.Model):
+    actor = models.ForeignKey(Actor, on_delete=models.SET_NULL, related_name='roles')
+    movie = models.ForeignKey(Movie, on_delete=models.CASCADE, related_name='roles')
